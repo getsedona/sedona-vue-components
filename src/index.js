612660e2435a1fd2bc2442adb59a3f2eee734633
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-03-11 17:56:01 +0300

    fix: fix imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 export * from './components/Feature'
 export * from './components/Card'
 export * from './components/Collapse'
-export * from './components/ProductCard'
 export * from './components/Developer'
+
+export * from './components/ProductCard'
+export * from './components/Price'
+
 export * from './components/Page'
+export * from './components/Scene'
 export * from './components/Grid'
