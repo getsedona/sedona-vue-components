593a769af790dccaa38196dbe73ac813ea9c302e
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-03-11 13:43:37 +0300

    chore: update dependencies

--- styleguide.config.js
+++ styleguide.config.js
@@ -3,63 +3,69 @@ const path = require("path");
 module.exports = {
   defaultExample: false,
   pagePerSection: true,
-  title: "Sedona Vue Components",
-  usageMode: "expand",
-  require: [path.join(__dirname, "src/assets/less/index.less"), path.join(__dirname, "router-mock.js")],
+  title: 'Sedona Vue Components',
+  usageMode: 'expand',
+  require: [
+    path.join(__dirname, 'src/assets/less/index.less'),
+    path.join(__dirname, 'router-mock.js'),
+  ],
   styles: {
     Logo: {
       logo: {
-        color: "#fff",
-        fontSize: 24
-      }
-    }
+        color: '#fff',
+        fontSize: 24,
+      },
+    },
   },
   template: {
     head: {
       links: [
         {
-          href: "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons",
-          rel: "stylesheet"
-        }
-      ]
-    }
+          href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons',
+          rel: 'stylesheet',
+        },
+      ],
+    },
   },
   serverPort: 3000,
   theme: {
     fontFamily: {
-      monospace: ["Menlo", "Consolas", '"Liberation Mono"', "monospace"]
+      monospace: ['Menlo', 'Consolas', '"Liberation Mono"', 'monospace'],
     },
     color: {
-      link: "#fff",
-      sidebarBackground: "#009688"
+      link: '#fff',
+      sidebarBackground: '#009688',
     },
     sidebarWidth: 250,
-    maxWidth: "1000px"
+    maxWidth: '1000px',
   },
   sections: [
     {
-      name: "Introduction",
-      content: "docs/introduction.md"
+      name: 'Introduction',
+      content: 'docs/introduction.md',
     },
     {
-      name: "Layout",
-      description: "Layout description",
-      content: "docs/layout.md",
-      components: ["src/components/Page/Page.ts"],
+      name: 'Layout',
+      description: 'Layout description',
+      content: 'docs/layout.md',
+      components: [
+        'src/components/Page/Page.js',
+        'src/components/Scene/Scene.js',
+      ],
       sectionDepth: 1,
       sections: [
         {
-          name: "Grid",
-          content: "docs/grid.md"
-        }
-      ]
+          name: 'Grid',
+          content: 'docs/grid.md',
+        },
+      ],
     },
     {
-      name: "UI Components",
-      content: "docs/ui.md",
-      components: ["./src/components/**/[A-Z]*.vue", "./src/components/**/[A-Z]*.js"],
+      name: 'UI Components',
+      content: 'docs/ui.md',
+      components: ['./src/components/**/[A-Z]*.vue', './src/components/**/[A-Z]*.js'],
       sectionDepth: 1,
-      ignore: "src/components/Page/Page.ts"
-    }
-  ]
-};
+      ignore: 'src/components/Page/Page.ts',
+    },
+  ],
+}
